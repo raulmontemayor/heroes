d49e60829215a8d2d55e786f93edfe3a0d5a973d
Raul Montemayor <raul.montemayor@grainger.com>
2021-08-05 19:42:57 -0500

    Lombok superbuilder was messing with the tests, changing to a manually created builder

--- Adapters/src/main/java/es/mindata/heroes/adapters/out/persistence/model/HeroModel.java
+++ Adapters/src/main/java/es/mindata/heroes/adapters/out/persistence/model/HeroModel.java
@@ -8,59 +8,83 @@ import javax.persistence.Id;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.mindata.heroes.entities.Hero;
-import lombok.experimental.SuperBuilder;
-
 
 @RestResource
 @Entity(name = "hero")
-@SuperBuilder
 public class HeroModel extends Hero {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Override
 	public Long getId() {
 		return super.getId();
 	}
-	
+
 	@Override
 	public String getFullName() {
 		return super.getFullName();
 	}
-	
+
 	@Override
 	public void setFullName(String fullName) {
 		super.setFullName(fullName);
 	}
-	
+
 	@Override
 	public String getAlterEgo() {
 		return super.getAlterEgo();
 	}
-	
+
 	@Override
 	public void setAlterEgo(String alterEgo) {
 		super.setAlterEgo(alterEgo);
 	}
-	
+
 	@Override
 	public String getSpecies() {
 		return super.getSpecies();
 	}
-	
+
 	@Override
 	public void setSpecies(String species) {
 		super.setSpecies(species);
 	}
-	
+
 	@Override
 	public String getUniverse() {
 		return super.getUniverse();
 	}
-	
+
 	@Override
 	public void setUniverse(String universe) {
 		super.setUniverse(universe);
 	}
-	
+
+	public static HeroModelBuilder builder() {
+		return new HeroModelBuilder(new HeroModel());
+	}
+
+	public static class HeroModelBuilder {
+
+		private HeroModel heroModel;
+
+		public HeroModelBuilder(HeroModel heroModel) {
+			this.heroModel = heroModel;
+		}
+
+		public HeroModelBuilder fullName(String fullName) {
+			heroModel.setFullName(fullName);
+			return this;
+		}
+
+		public HeroModelBuilder universe(String universe) {
+			heroModel.setUniverse(universe);
+			return this;
+		}
+
+		public HeroModel build() {
+			return heroModel;
+		}
+	}
+
 }
--- Domain/src/main/java/es/mindata/heroes/entities/Hero.java
+++ Domain/src/main/java/es/mindata/heroes/entities/Hero.java
@@ -1,10 +1,8 @@
 package es.mindata.heroes.entities;
 
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
 
 @Data
-@SuperBuilder
 public class Hero {
 	
 	private Long id;
